736cd0a15d425366bceef7d9681abb2d790101f9
Santiagoo <santiagominotta2913@hotmail.com>
2026-03-18 19:50:07 -0700

    Set inventory dictionary

---
+++ harold_coursework1.py
@@ -0,0 +1,17 @@
+inventory = {
+    101: {
+        "name": "Laptop",
+        "quantity": 5,
+        "price": 799.99
+    },
+    102: {
+        "name": "Mouse",
+        "quantity": 10,
+        "price": 25.50
+    },
+    103: {
+        "name": "Keyword",
+        "quantity": 8,
+        "price": 20.15
+    }
+}
