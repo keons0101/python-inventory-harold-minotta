6e074d50a82463e6be40cab721f007931c0c751c
Santiagoo <santiagominotta2913@hotmail.com>
2026-03-19 15:54:51 -0700

    Add functionality to input new inventory items

--- harold_coursework1.py
+++ harold_coursework1.py
@@ -10,8 +10,26 @@ inventory = {
         "price": 25.50
     },
     103: {
-        "name": "Keyword",
+        "name": "Keyboard",
         "quantity": 8,
         "price": 20.15
     }
 }
+print("Welcome to Bigstore----------------------------------------")
+
+new_id = int(input("Enter product ID: "))
+new_name = input("Enter product name: ").strip().title()
+new_quantity = int(input("Enter quantity: "))
+new_price = float(input("Enter product price: "))
+
+inventory[new_id] = {
+    "name": new_name,
+    "quantity": new_quantity,
+    "price": new_price
+}
+
+print("Item added successfully!!!")
+print(f"ID: {new_id} | Name: {new_name} | Quantity: {new_quantity} | Price: ${new_price:.2f}")
+print("-------------------------------------------------------------------------------------------")
+for product_id, product_data in inventory.items():
+    print(f"ID: {product_id} | Name: {product_data['name']} | Quantity: {product_data['quantity']} | Price: ${product_data['price']:.2f}")
